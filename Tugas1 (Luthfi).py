2df24ee910a4273fe76847dc07c47b0cc029bb10
luthfien <56451391+luthfien@users.noreply.github.com>
2019-12-28 16:26:01 +0700

    Add files via upload

---
+++ Tugas1 (Luthfi).py
@@ -0,0 +1,11 @@
+#INI Variabel & Nilai
+
+namalengkap = "Luthfi Eka Naufal"
+namapanggilan = "Luthfi"
+npm = "311910730"
+tempattgllahir = "Bekasi"
+umur = "19"
+nohp = "+62895-1966-7536"
+alamat = "Perum Mutiara Bekasi Jaya Blok D14 No.24"
+
+print("Assalamu'alaikum Wr. Wb, \n Let me introduce my self. My name is",namalengkap,", but you can call me",namapanggilan,".\n My NPM is",npm,".\n I was born in",tempattgllahir,",and i am",umur,"years old.\n I am very glad if you want to invite my house in",alamat,".\n So, don't forget to call me before with number",nohp,"\n Thank You")
